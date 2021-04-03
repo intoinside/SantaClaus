ecff1be47094b0d9f704944a4067636911f83220
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2021-04-03 18:24:04 +0200

    Added sprite without animation

--- sprites.asm
+++ sprites.asm
@@ -7,35 +7,55 @@ SetupSprites: {
 		sta SCREEN_RAM + $03f8 + $02
 		sta SCREEN_RAM + $03f8 + $03
 		sta SCREEN_RAM + $03f8 + $04
+		lda #$31
+		sta SCREEN_RAM + $03f8 + $05
+		lda #$35
+		sta SCREEN_RAM + $03f8 + $06
+		lda #$34
+		sta SCREEN_RAM + $03f8 + $07
 
-		lda #%00011111
+		lda #%11111111
 		sta VIC.SPRITE_MULTICOLOR
 
 		lda #$02
 		sta $d027			// Sprite #0 color
-//		lda #$02
+		lda #$09
 		sta $d028			// Sprite #1 color
 		sta $d029
 		sta $d02a
 		sta $d02b
-
+		lda #$05
+		sta $d02c
 		lda #$00
+		sta $d02d
+		lda #$01
+		sta $d02e
+
+		lda #$02
 		sta VIC.SPRITE_EXTRACOLOR1
 		lda #$08
 		sta VIC.SPRITE_EXTRACOLOR2
 
-		lda #%00011111
+		lda #%11111111
 		sta VIC.SPRITE_ENABLE
 
+// Y Positioning 
 		lda #$80
 		sta $d001
 		sta $d003
 		sta $d005
 		sta $d007
 		sta $d009
+		sta $d00b
 
+		lda #$dc
+		sta $d00d
+		sta $d00f
+
+// X Positioning 
 		lda #$30
 		sta $d000
+		sta $d00a
 		lda #$48
 		sta $d002
 		lda #$5a
@@ -44,5 +64,10 @@ SetupSprites: {
 		sta $d006
 		lda #$7e
 		sta $d008
+
+		lda #$90
+		sta $d00c
+		sta $d00e
+
 		rts
 }
